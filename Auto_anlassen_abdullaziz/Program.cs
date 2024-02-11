e422cf53bf952ce10cd2e88bccf5d5e218a0f437
Abdullaziz Hassen Abdella <abdullaziz.hassenabdella@ksb-sg.ch>
2024-02-11 21:22:02 +0100

    Add project files.

---
+++ Auto_anlassen_abdullaziz/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Auto_anlassen_abdullaziz
+{
+    
+
+    class Auto
+    {
+        public void Anlassen()
+        {
+            Console.WriteLine("Auto anlassen");
+            Motor motor = new Motor();
+            motor.Starten();
+        }
+    }
+
+    class Motor
+    {
+        public void Starten()
+        {
+            Console.WriteLine("Motor gestartet");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto meinAuto = new Auto();
+            meinAuto.Anlassen();
+
+            Console.ReadLine();
+        }
+    }
+}
